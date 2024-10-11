cac44626ccedb35e885004e8144c13dd7706a97a
pendragonarthur <Arthur Quaresma>
2024-10-10 23:56:39 -0300

    feat: added backward prop

--- functions.py
+++ functions.py
@@ -1,17 +1,42 @@
 import numpy as np
 
 def loss_function(true_pred, model_pred):
-    sample = true_pred.shape[1]
-    loss = -np.sum(true_pred * np.log(model_pred)) / sample
+    m = true_pred.shape[1] # m = numero de amostrar no conjunto de dados
+    loss = -np.sum(true_pred * np.log(model_pred)) / m
     return loss
 
-def softmax(x)
+def softmax(x):
     exps = np.exp(x - np.max(x))
     return exps / np.sum(exps, axis=0)
 
+def relu(x):
+    return np.maximum(0, x)
+
+def initialize_weights(neurons, inputs): # inicialização HE
+    dev = np.sqrt(2 / inputs) # desvio padrão
+    weights = np.random.randn(neurons, inputs) * dev 
+    return weights
+
+def initialize_biases(neurons):
+    bias_matrix = np.zeros((neurons, 1)) # cria uma matriz de 0 de 'neurons' linhas e 1 coluna
+    return bias_matrix
+
+
 def forward_prop(weights, inputs, biases):
-    weighted_sum = np.dot(weights, inputs) + biases
+    weighted_sum = relu(np.dot(weights, inputs) + biases)
     return weighted_sum
 
-def backward_prop(layers, learning_rate): 
-    pass
+def backward_prop(final_output, hidden_output, true_pred, learning_rate, weights, biases, inputs):
+    error_output = final_output - true_pred
+
+    error_hidden = np.dot(weights['output'].T, error_output) * (hidden_output * (1 - hidden_output))
+
+    weights['output'] -= learning_rate * np.dot(error_output, hidden_output.T)
+    biases['output'] -= learning_rate * np.sum(error_output, axis=1, keepdims=True)
+
+    weights['hidden'] -= learning_rate * np.dot(error_hidden, inputs.T)
+    biases['hidden'] -= learning_rate * np.sum(error_hidden, axis=1, keepdims=True)
+
+    return weights, biases 
+
+    
--- neural_network.py
+++ neural_network.py
@@ -1,23 +1,25 @@
 import numpy as np
-from functions import forward_prop
+from functions import forward_prop, softmax, initialize_biases, initialize_weights
 
 def input_layer(neurons):
     pass
 
 def hidden_layers(inputs, neurons, layers=2):
+    weights_dict = []
+    biases_dict = []
+
     for _ in range(layers):
-        weights = np.random.uniform(-1.0, 1.0, (neurons, len(inputs)))
-        print(weights)
-        bias = np.random.uniform(-1.0, 1.0, neurons)
-        print(bias)
+        weights = initialize_weights(neurons, inputs)
+        bias = initialize_biases(neurons)
         layer_output = forward_prop(weights, neurons, bias)
-    return layer_output
+
+        weights_dict.append(weights)
+        biases_dict.append(bias)        
+    return layer_output, weights_dict, biases_dict
 
 def output_layer(inputs, neurons=10):
     weights = np.random.uniform(-1.0, 1.0, (neurons, len(inputs)))
     bias = np.random.uniform(-1.0, 1.0, neurons)
-    final_output = forward_prop(weights, neurons, bias)
-    return final_output
-
-
-    
+    logits = forward_prop(weights, neurons, bias)
+    final_output = softmax(logits)
+    return final_output, weights, bias
